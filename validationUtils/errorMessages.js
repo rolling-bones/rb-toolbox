71bdec31bde593dc16d23f07a10498d3e579fb99
sereigh <kb9286@gmail.com>
2023-10-21 14:42:05 -0600

    adding error messages

---
+++ validationUtils/errorMessages.js
@@ -0,0 +1,10 @@
+/**
+ * General error message.
+ */
+export const invalidInput = '[Invalid input] ';
+
+
+/**
+ * String error message.
+ */
+export const notAString = 'Expected a string, but received: ';
